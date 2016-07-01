87eb1def449ce12e0b1520e6b18b853c187ce8f0
Nanquitas <nath.doidi@gmail.com>
2016-07-01 17:32:46 +0200

    Corrected a minor bug in the macros

--- Sources/cheats.h
+++ Sources/cheats.h
@@ -3,8 +3,8 @@
 
 #include "../Includes/plugin.h"
 
-#define		add_search_replace(find, replace)	g_find[g_i] = find; g_replace[g_i] = replace; g_i++
-#define		reset_search()						memset(g_find, 0, sizeof(g_find)); memset(g_replace, 0, sizeof(g_find)); g_i = 0
+#define		add_search_replace(find, replace) g_find[g_i] = find; g_replace[g_i] = replace; g_i++
+#define		reset_search()                    memset(g_find, 0, sizeof(g_find)); memset(g_replace, 0, sizeof(g_replace)); g_i = 0
 
 /*
 ** Helpers
