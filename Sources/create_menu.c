886964006abfa4ed9a924974aaae6bca9daded8a
Sir. Beethoven <SirBeethoven@users.noreply.github.com>
2016-07-07 10:41:54 -0500

    Moved Environment Codes down on the menu

--- Sources/create_menu.c
+++ Sources/create_menu.c
@@ -9,15 +9,7 @@ void	usa_menu(void)
 {
 	t_spoil spoiler;
 
-	new_unselectable_entry("ACNL NTR Cheats Ver 2.1.1 USA");
-	spoiler = new_spoiler("Enviroment Codes");
-		new_spoiled_entry(spoiler, "Water all flowers", quench_usa);
-		new_spoiled_entry(spoiler, "Seeder", seed_usa);
-		new_spoiled_entry(spoiler, "Search and Replace", search_usa);
-		new_spoiled_entry(spoiler, "Grass", grass_usa);
-		new_spoiled_entry(spoiler, "Desert", desert_usa);
-		new_spoiled_entry(spoiler, "Remove all weeds", weeder_usa);
-		new_spoiled_entry(spoiler, "Instant Tree", tree_usa);
+	new_unselectable_entry("ACNL Cheats 2.1.1");
 	spoiler = new_spoiler("Inventory Codes");
 		new_spoiled_entry(spoiler, "Text to Item", text2item_usa);
 		new_spoiled_entry(spoiler, "Duplication", duplicate_usa);
@@ -26,6 +18,14 @@ void	usa_menu(void)
 		new_spoiled_entry(spoiler, "Teleport", teleport_usa);
 		new_spoiled_entry(spoiler, "Coordinates Modifier", coord_usa);
 		new_spoiled_entry(spoiler, "Speed Hack", speed_usa);
+	spoiler = new_spoiler("Enviroment Codes");
+		new_spoiled_entry(spoiler, "Water all flowers", quench_usa);
+		new_spoiled_entry(spoiler, "Seeder", seed_usa);
+		new_spoiled_entry(spoiler, "Search and Replace", search_usa);
+		new_spoiled_entry(spoiler, "Grass", grass_usa);
+		new_spoiled_entry(spoiler, "Desert", desert_usa);
+		new_spoiled_entry(spoiler, "Remove all weeds", weeder_usa);
+		new_spoiled_entry(spoiler, "Instant Tree", tree_usa);
 	spoiler = new_spoiler("Nookling Upgrades");
 		new_spoiled_entry(spoiler, "T&T Mart", nook1_usa);
 		new_spoiled_entry(spoiler, "Super T&T", nook2_usa);
@@ -39,15 +39,7 @@ void	eur_menu(void)
 {
 	t_spoil spoiler;
 
-	new_unselectable_entry("ACNL NTR Cheats Ver 2.1.1 EUR");
-	spoiler = new_spoiler("Enviroment Codes");
-		new_spoiled_entry(spoiler, "Water all flowers", quench_eur);
-		new_spoiled_entry(spoiler, "Seeder", seed_eur);
-		new_spoiled_entry(spoiler, "Search and Replace", search_eur);
-		new_spoiled_entry(spoiler, "Grass", grass_eur);
-		new_spoiled_entry(spoiler, "Desert", desert_eur);
-		new_spoiled_entry(spoiler, "Remove all weeds", weeder_eur);
-		new_spoiled_entry(spoiler, "Instant Tree", tree_eur);
+	new_unselectable_entry("ACNL Cheats 2.1.1");
 	spoiler = new_spoiler("Inventory Codes");
 		new_spoiled_entry(spoiler, "Text to Item", text2item_eur);
 		new_spoiled_entry(spoiler, "Duplication", duplicate_eur);
@@ -56,6 +48,14 @@ void	eur_menu(void)
 		new_spoiled_entry(spoiler, "Teleport", teleport_eur);
 		new_spoiled_entry(spoiler, "Coordinates Modifier", coord_eur);
 		new_spoiled_entry(spoiler, "Speed Hack", speed_eur);
+	spoiler = new_spoiler("Enviroment Codes");
+		new_spoiled_entry(spoiler, "Water all flowers", quench_eur);
+		new_spoiled_entry(spoiler, "Seeder", seed_eur);
+		new_spoiled_entry(spoiler, "Search and Replace", search_eur);
+		new_spoiled_entry(spoiler, "Grass", grass_eur);
+		new_spoiled_entry(spoiler, "Desert", desert_eur);
+		new_spoiled_entry(spoiler, "Remove all weeds", weeder_eur);
+		new_spoiled_entry(spoiler, "Instant Tree", tree_eur);
 	spoiler = new_spoiler("Nookling Upgrades");
 		new_spoiled_entry(spoiler, "T&T Mart", nook1_eur);
 		new_spoiled_entry(spoiler, "Super T&T", nook2_eur);
