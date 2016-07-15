8b683f1e46539526c171962237e47428b59ec61d
Nanquitas <nath.doidi+github@gmail.com>
2016-07-15 11:46:41 +0200

    Clean common.h, correct wait_keys_released

--- Includes/common.h
+++ Includes/common.h
@@ -8,24 +8,10 @@ extern vu32 *pad_base;
 
 void	set_hid_address(u32 address);
 
-/*
 static inline int is_pressed(u32 keys)
 {
-	u32 i = 0x13000;
-
-	while (--i > 0 && ((((*(vu32*)(IoBasePad) ^ 0xFFF) & 0xFFF) & keys) == keys));
-	if (i == 0)
-		return(1);
-	return (0);
-}*/
-
-static inline int is_pressed(u32 keys)
-{
-	//u32 i = 0x13000;
-
 	if (pad_base != NULL)
 	{
-		//scanHID();
 		if (((hidKeysDown() & keys) == keys))
 			return (1);
 	}
@@ -37,13 +23,8 @@ static inline int is_pressed(u32 keys)
 
 static inline int any_is_pressed(u32 keys)
 {
-	//u32 i = 0x13000;
-
-	//while (--i > 0 && ((*(vu32*)(IoBasePad) ^ 0xFFF) & 0xFFF) & keys);
-	//if (i == 0)
 	if (pad_base != NULL)
 	{
-		//scanHID();
 		if (((hidKeysDown() & keys)))
 			return (1);
 	}
@@ -62,7 +43,7 @@ static inline int wait_keys(u32 keys)
 static inline void wait_keys_released(u32 keys)
 {
 	while (1)
-		if (!(is_pressed(keys)))
+		if (!(any_is_pressed(keys)))
 			return;
 }
 
