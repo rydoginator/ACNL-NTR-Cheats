83b48be8a54019f11824f927ea03b355334be17e
Nathanaël DOIDI <nath.doidi@gmail.com>
2016-11-19 13:55:39 +0100

    Speed up quench algorithm

--- Sources/cheats.c
+++ Sources/cheats.c
@@ -314,94 +314,6 @@ void    weeder(void)
 
 void quench(void)
 {
-    // define flowers (include wilted)
-    static const u16 flowers[] =
-    {
-        // src, replace
-        0x009F, 0x009F, // Red Tulips
-        0x00A0, 0x00A0, // White Tulips
-        0x00A1, 0x00A1, // Yellow Tulips
-        0x00A2, 0x00A2, // Pink Tulips
-        0x00A3, 0x00A3, // Purple Tulips
-        0x00A4, 0x00A4, // Black Tulips
-        0x00A5, 0x00A5, // Orange Tulips
-        0x00A6, 0x00A6, // White Pansies
-        0x00A7, 0x00A7, // Yellow Pansies
-        0x00A8, 0x00A8, // Red Pansies
-        0x00A9, 0x00A9, // Purple Pansies
-        0x00AA, 0x00AA, // Orange Pansies
-        0x00AB, 0x00AB, // Blue Pansies
-        0x00AC, 0x00AC, // White Comsos
-        0x00AD, 0x00AD, // Red Cosmos
-        0x00AE, 0x00AE, // Sun Cosmos
-        0x00AF, 0x00AF, // Pink Cosmos
-        0x00B0, 0x00B0, // Orange Cosmos
-        0x00B1, 0x00B1, // Black Cosmos
-        0x00B2, 0x00B2, // Red Roses
-        0x00B3, 0x00B3, // White Roses
-        0x00B4, 0x00B4, // Yellow Roses
-        0x00B5, 0x00B5, // Pink Roses
-        0x00B6, 0x00B6, // Orange Roses
-        0x00B7, 0x00B7, // Purple Roses
-        0x00B8, 0x00B8, // Black Roses
-        0x00B9, 0x00B9, // Blue Roses
-        0x00BB, 0x00BB, // Red Carnations
-        0x00BC, 0x00BC, // Pink Carnations
-        0x00BD, 0x00BD, // White Carnations
-        0x00BE, 0x00BE, // White Lilies
-        0x00BF, 0x00BF, // Yellow Lilies
-        0x00C0, 0x00C0, // Red Lilies
-        0x00C1, 0x00C1, // Orange Lilies
-        0x00C2, 0x00C2, // Pink Lilies
-        0x00C3, 0x00C3, // Black Lilies
-        0x00C4, 0x00C4, // Purple Violets
-        0x00C5, 0x00C5, // Blue Violets
-        0x00C6, 0x00C6, // White Violets
-        0x00C7, 0x00C7, // Yellow Violets
-        0x00C8, 0x00C8, // Jacob's Ladder
-        0x00CE, 0x009F, // Red Tulips (Wilted)
-        0x00CF, 0x00A0, // White Tulips (Wilted)
-        0x00D0, 0x00A1, // Yellow Tulips (Wilted)
-        0x00D1, 0x00A2, // Pink Tulips (Wilted)
-        0x00D2, 0x00A3, // Purple Tulips (Wilted)
-        0x00D3, 0x00A4, // Black Tulips (Wilted)
-        0x00D4, 0x00A5, // Orange Tulips (Wilted)
-        0x00D5, 0x00A6, // White Pansies (Wilted)
-        0x00D6, 0x00A7, // Yellow Pansies (Wilted)
-        0x00D7, 0x00A8, // Red Pansies (Wilted)
-        0x00D8, 0x00A9, // Purple Pansies (Wilted)
-        0x00D9, 0x00AA, // Orange Pansies (Wilted)
-        0x00DA, 0x00AB, // Blue Pansies (Wilted)
-        0x00DB, 0x00AC, // White Cosmos (Wilted)
-        0x00DC, 0x00AD, // Red Cosmos (Wilted)
-        0x00DD, 0x00AE, // Sun Cosmos (Wilted)
-        0x00DE, 0x00AF, // Pink Cosmos (Wilted)
-        0x00DF, 0x00B0, // Orange Cosmos (Wilted)
-        0x00E0, 0x00B1, // Black Cosmos (Wilted)
-        0x00E1, 0x00B2, // Red Roses (Wilted)
-        0x00E2, 0x00B3, // White Roses (Wilted)
-        0x00E3, 0x00B4, // Yellow Roses (Wilted)
-        0x00E4, 0x00B5, // Pink Roses (Wilted)
-        0x00E5, 0x00B6, // Orange Roses (Wilted)
-        0x00E6, 0x00B7, // Purple Roses (Wilted)
-        0x00E7, 0x00B8, // Black Roses (Wilted)
-        0x00E8, 0x00B9, // Blue Roses (Wilted)
-        0x00E9, 0x00BA, // Gold Roses (Wilted)
-        0x00EA, 0x00BB, // Red Carnations (Wilted)
-        0x00EB, 0x00BC, // Pink Carnations (Wilted)
-        0x00EC, 0x00BD, // White Carnations (Wilted)
-        0x00ED, 0x00BE, // White Lilies (Wilted)
-        0x00EE, 0x00BF, // Yellow Lilies (Wilted)
-        0x00EF, 0x00C0, // Red Lilies (Wilted)
-        0x00F0, 0x00C1, // Orange Lilies (Wilted)
-        0x00F1, 0x00C2, // Pink Lilies (Wilted)
-        0x00F2, 0x00C3, // Black Lilies (Wilted)
-        0x00F3, 0x00C4, // Purple Violets (Wilted)
-        0x00F4, 0x00C5, // Blue Violets (Wilted)
-        0x00F5, 0x00C6, // White Violets (Wilted)
-        0x00F6, 0x00C7, // Yellow Violets (Wilted)
-        0x00F7, 0x00C8, // Jacob's Ladder (Wilted)
-    };
     int     i;
     u32     address;
     u32     item;
@@ -411,17 +323,16 @@ void quench(void)
     for (address = g_town_items; address < g_town_items + RANGE_TOWN_ITEMS; address += ITEM_BYTES)
     {
         item = READU16(address);
-        // find flowers
-        for(i = 0; i < sizeof(flowers) / sizeof(u16); i += 2)
+        // Deal with wilted flower
+        if (item >= 0xCE && item <= 0xF7)
         {
-            if(item == flowers[i])
-            {
-                // replace item
-                WRITEU16(address, flowers[i + 1]);
-                // add water flag
-                WRITEU16(address + 0x02, 0x4000);
-                break;
-            }
+            item -= 0x2F;
+        }
+        // If item is a flower add water flag
+        if (item >= 0x9F && item <= 0xC8)
+        {
+            item |= 0x40000000;
+            WRITEU32(address, item);
         }
     }
 }
