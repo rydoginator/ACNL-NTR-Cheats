4ecb03fbb56bf09a11b7bcff046656e23b52ca3a
Ryan Plante <rplante565@gmail.com>
2016-11-19 23:50:09 -0500

    Bug fixes and offset correction

--- Sources/helpers.c
+++ Sources/helpers.c
@@ -3,6 +3,7 @@
 extern u32  g_find[100];
 extern u32  g_replace[100];
 extern int  g_i;
+extern u32  g_input_text_buffer;
 
 void    find_and_replace_multiple(void *start_addr, u32 length)
 {
@@ -35,11 +36,16 @@ void    keep_it_off(void)
 
 void    retrieve_input_string(char *output, int size)
 {
-    const u32   input_buffer_address = 0x32cb0f60;
+    // TODO: properly managing wide char
+    char    buffer[0x100];
+    int     i;
 
     if (!output || size < 1)
         goto error;
-    memcpy(output, (void *)input_buffer_address, size);
+    size *= 2;
+    memcpy(buffer, (void *)g_input_text_buffer, size > 0x100 ? 0x100 : size);
+    for (i = 0; i < size; i++)
+        *output++ = *(buffer + i++);
     error:
         return;
 }
@@ -48,7 +54,6 @@ void    get_input_id(int *first, int *second)
 {
     char    buffer[13];
     char    *pointer_id;
-    int     input;
     int     hex_pattern;
 
     if (!first)
@@ -104,4 +109,14 @@ void    get_input_id(int *first, int *second)
     *first = (int)strtoul(pointer_id, NULL, 16);
 error:
     return;
+}
+
+bool    match(const char *str, const char *pattern)
+{
+    int     pattern_size;
+
+    pattern_size = strlen(pattern);
+    if (strncmp(str, pattern, pattern_size) == 0)
+        return (true);
+    return (false);
 }
