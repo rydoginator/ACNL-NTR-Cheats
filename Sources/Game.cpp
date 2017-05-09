64300616d99be67f8f70f43d2dbcaf3214292a0e
Nanquitas <nath.doidi@gmail.com>
2017-05-09 20:04:17 +0200

    Game: add GetWorldOffset and GetItem

--- Sources/Game.cpp
+++ Sources/Game.cpp
@@ -8,16 +8,71 @@ namespace CTRPluginFramework
     // Initialize properties
     void    Game::Initialize(void)
     {
+        ClubItem = reinterpret_cast<u32 *>(AutoRegion(USA_CLUB_ADDR, TO_EUR(USA_CLUB_ADDR), TO_JAP(USA_CLUB_ADDR))());
         Garden = AutoRegion(USA_GARDEN, TO_EUR(USA_GARDEN), TO_JAP(USA_GARDEN))();
         Gravity = AutoRegion(USA_GRAVITY_OUT_ADDR, TO_EUR(USA_GRAVITY_OUT_ADDR), TO_JAP(USA_GRAVITY_OUT_ADDR))();
+        MainStreetPos = reinterpret_cast<Position *>(AutoRegion(USA_MAINSTREET_X, TO_EUR(USA_MAINSTREET_X), TO_JAP(USA_MAINSTREET_X))());
+        Room = reinterpret_cast<u8 *>(AutoRegion(USA_ROOM_ID_ADDR, EUR_ROOM_ID_ADDR, JAP_ROOM_ID_ADDR)());
         TCPImage = Garden + 0x5758;
+        TownItem = reinterpret_cast<u32 *>(AutoRegion(USA_TOWN_ITEMS_ADDR, TO_EUR(USA_TOWN_ITEMS_ADDR), TO_JAP(USA_TOWN_ITEMS_ADDR))());
         Velocity = AutoRegion(USA_VELOCITY_ADDR, TO_EUR(USA_VELOCITY_ADDR), TO_JAP(USA_VELOCITY_ADDR))();
-
+        WorldPos = reinterpret_cast<Position *>(AutoRegion(USA_WORLD_X_ADDR, TO_EUR(USA_WORLD_X_ADDR), TO_JAP(USA_WORLD_X_ADDR))());
     }
 
-    u32     Game::Garden = 0;
-    u32     Game::Gravity = 0;
-    u32     Game::TCPImage = 0;
-    u32     Game::Velocity = 0;
-    
+    u32     Game::GetWorldOffset(void)
+    {
+        u8      room = *Room;
+        Position *pos = room == 0x01 ? MainStreetPos : WorldPos;
+
+        u32     x = pos->x;
+        u32     y = pos->y;
+        u32     reg0;
+        u32     reg1;
+
+        if (room == 0x01)
+        {
+            reg0 = x % 0x10;
+            x /= 0x10;
+            reg1 = y % 0x10;
+            y /= 0x10;
+            reg0 *= 0x4;
+            reg1 *= 0x40;
+            x *= 0x400;
+            y *= 0x1400;
+        }
+        else
+        {
+            x -= 0x10;
+            y -= 0x10;
+            reg0 = x % 0x10;
+            x /= 0x10;
+            reg1 = y % 0x10;
+            y /= 0x10;
+            reg0 *= 0x4;
+            reg1 *= 0x40;
+            x *= 0x400;
+            y *= 0x1400;
+        }
+        return (reg0 + reg1 + x + y);
+    }
+
+    u32     *Game::GetItem(void)
+    {
+        u32  *items;
+
+        if (*Room == 0x0) items = TownItem;
+        else if (*Room == 0x6F) items = ClubItem;
+        else return (nullptr);
+        
+        return (items + GetWorldOffset());
+    }
+
+    u32         *Game::ClubItem = nullptr;
+    u32         Game::Garden = 0;
+    u32         Game::Gravity = 0;
+    Position    *Game::MainStreetPos = nullptr;
+    u32         Game::TCPImage = 0;
+    u32         *Game::TownItem = nullptr;
+    u32         Game::Velocity = 0;
+    Position    *Game::WorldPos = nullptr;    
 }
--- Sources/Game.hpp
+++ Sources/Game.hpp
@@ -5,19 +5,36 @@
 
 namespace CTRPluginFramework
 {
+    struct Position
+    {
+        u32     x;
+        u32     y;
+    };
+
     class Game
     {
     public:
 
         // Initialize properties
-        static void    Initialize(void);
+        static void     Initialize(void);
+
+        // Return current world offset
+        static u32      GetWorldOffset(void);
+        // Return item's address at current position.
+        // nullptr is returned is Room is unknown
+        static u32      *GetItem(void);
 
         // Properties
 
+        static u32      *ClubItem;      ///< Address of the first item in club
         static u32      Garden;         ///< Address of the garden
         static u32      Gravity;        ///< Address of gravity
+        static Position *MainStreetPos; ///< Position of the player on MainStreet
+        static u8       *Room;          ///< Address of the current room
         static u32      TCPImage;       ///< Address of the TCP picture
+        static u32      *TownItem;      ///< Address of the first item in town
         static u32      Velocity;       ///< Address of velocity
+        static Position *WorldPos;      ///< Position of the player in the world
         
 
     };
