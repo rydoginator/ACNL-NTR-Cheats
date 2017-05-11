2789de526c9df4242813cd276d9fe0bab777676a
Nanquitas <nath.doidi@gmail.com>
2017-05-11 10:18:59 +0200

    Implement HoldKey

---
+++ Sources/Helpers/HoldKey.cpp
@@ -0,0 +1,33 @@
+#include "HoldKey.hpp"
+
+namespace CTRPluginFramework
+{
+    HoldKey::HoldKey(u32 keys, Time holdTime) :
+        _goal(holdTime), _isHold(false), _keys(keys)
+    {
+    }
+
+    bool    HoldKey::operator()(void)
+    {
+        bool isHold = Controller::IsKeysDown(_keys);
+
+        // If currently hold
+        if (isHold && _isHold && _timer.HasTimePassed(_goal))
+        {
+            _isHold = false;
+            return (true);
+        }
+        
+        
+
+        if (isHold && !_isHold)
+        {
+            _isHold = true;
+            _timer.Restart();
+        }
+        else if (_isHold && !isHold)
+            _isHold = false;
+
+        return (false);
+    }
+}
