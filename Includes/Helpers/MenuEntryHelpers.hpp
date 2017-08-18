df7646867ffcc9849706625bc49565ea2a438d70
Nanquitas <nath.doidi@gmail.com>
2017-08-18 23:25:16 +0200

    New template with default value for GetArg

--- Includes/Helpers/MenuEntryHelpers.hpp
+++ Includes/Helpers/MenuEntryHelpers.hpp
@@ -25,6 +25,28 @@ namespace CTRPluginFramework
 
         return (arg);
     }
+
+    /**
+    * \brief Return the arg of an entry \n
+    * If the arg doesn't exist (nullptr) a new one is created calling the default type constructor
+    * \tparam T The type of the arg
+    * \param entry The entry to get the arg from
+    * \param defaultValue The value to set to a newly created arg
+    * \return A pointer to the arg (like reinterpret_cast<T *>(entry->GetArg()))
+    */
+    template <typename T>
+    T   *GetArg(MenuEntry *entry, T defaultValue)
+    {
+        T   *arg = reinterpret_cast<T *>(entry->GetArg());
+
+        if (arg == nullptr)
+        {
+            arg = new T(defaultValue);
+            entry->SetArg(arg);
+        }
+
+        return (arg);
+    }
 }
 
 #endif
