f65580e1a8c6d1a3bcb3874c785acd0eb7351e6f
Nanquitas <nath.doidi@gmail.com>
2017-08-25 11:16:00 +0200

    Update menu's declaration

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -128,81 +128,84 @@ namespace CTRPluginFramework
         ** Garden
         ********************/
 
-        MenuFolder *folder = new MenuFolder("Save File Codes");
+        menu += new MenuFolder("Save File Codes", std::vector<MenuEntry *>(
         {
-            *folder += new MenuEntry("Set name to...", nullptr, SetNameTo);
-            *folder += new MenuEntry("Save Dumper", nullptr, GardenDumper, "Select the keyboard icon to start dumping your save file.");
-            *folder += new MenuEntry("Save Restore", nullptr, GardenRestore, "Select this icon to open file picker to restore from your previously dumped saves");
-            *folder += new MenuEntry("Change Town Fruit to...", nullptr, ChangeNativeFruit, "Special thanks to Mega Mew and Scotline");
-            *folder += new MenuEntry("Change Town Grass to...", nullptr, ChangeGrass, "Special thanks to Mega Mew and Scotline");
-            *folder += new MenuEntry("Real Time Building Placer", BuildingPlacer, "Press R + D Pad down to place a building by ID directly where you are standing\nPress R + D pad up to place a building where you are standing.");
-        }
-        menu += folder;
+            new MenuEntry("Set name to...", nullptr, SetNameTo),
+            new MenuEntry("Save Dumper", nullptr, GardenDumper, "Select the keyboard icon to start dumping your save file."),
+            new MenuEntry("Save Restore", nullptr, GardenRestore, "Select this icon to open file picker to restore from your previously dumped saves"),
+            new MenuEntry("Change Town Fruit to...", nullptr, ChangeNativeFruit, "Special thanks to Mega Mew and Scotline"),
+            new MenuEntry("Change Town Grass to...", nullptr, ChangeGrass, "Special thanks to Mega Mew and Scotline"),
+            new MenuEntry("Real Time Building Placer", BuildingPlacer, "Press R + D Pad down to place a building by ID directly where you are standing\nPress R + D pad up to place a building where you are standing."),
+
+        }));
+
 
         /*
         ** Movement
         ********************/
 
-        folder = new MenuFolder("Movement Codes");
+        menu += new MenuFolder("Movement Codes", std::vector<MenuEntry *>(
         {
-            *folder += new MenuEntry("Coordinates Modifier", CoordinateModifier, "Press \uE000 and D Pad in the direction that you want to move.");
-            *folder += new MenuEntry("Touch Coordinates", TouchCoordinates, "Touch the map to teleport your character there.");
-            *folder += new MenuEntry("Teleport", Teleporter, "Press \uE001 and \uE079 to save your location, \uE001 and \uE07A to teleport back to the location. Use \uE052 or \uE053 to use multiple locations!");
-            *folder += new MenuEntry("Walk Over Things", WalkOverThings, "Press \uE052 and \uE079 to enable walking through stuff, \uE052 and \uE07A to disable walking through stuff.");
-            *folder += new MenuEntry("Speed Hack", SpeedHack, SpeedHackEditor, "Change how fast you want to go with the keyboard icon\nCredits to Mega Mew for this cheat");
-            *folder += new MenuEntry("Moon Jump", MoonJump, "Press \uE052 and \uE079 to go higher and \uE07A to go lower.");
-        }
-        menu += folder;
+            new MenuEntry("Coordinates Modifier", CoordinateModifier, "Press \uE000 and D Pad in the direction that you want to move."),
+            new MenuEntry("Touch Coordinates", TouchCoordinates, "Touch the map to teleport your character there."),
+            new MenuEntry("Teleport", Teleporter, "Press \uE001 and \uE079 to save your location, \uE001 and \uE07A to teleport back to the location. Use \uE052 or \uE053 to use multiple locations!"),
+            new MenuEntry("Walk Over Things", WalkOverThings, "Press \uE052 and \uE079 to enable walking through stuff, \uE052 and \uE07A to disable walking through stuff."),
+            new MenuEntry("Speed Hack", SpeedHack, SpeedHackEditor, "Change how fast you want to go with the keyboard icon\nCredits to Mega Mew for this cheat"),
+            new MenuEntry("Moon Jump", MoonJump, "Press \uE052 and \uE079 to go higher and \uE07A to go lower.")
+        }));
 
-        folder = new MenuFolder("Main Street Codes");
+        menu += new MenuFolder("Main Street Codes", std::vector<MenuEntry *>(
         {
-            *folder += new MenuEntry("Nookling Upgrades", nullptr, NooklingKeyboard, "Press the keyboard icon to change which upgrade the Nooklings have");
-            *folder += new MenuEntry("Fill out Catalog", FillCatalog, "Fill out the catalog in Nookling's shop");
-            *folder += new MenuEntry("Fill out Main Street", FillMainStreet, "Unlocks all the Main Street buildings except Leif + Nooklings");
-            *folder += new MenuEntry("Catalog to Pockets", CatalogToPockets, "Press " FONT_L " and " FONT_A " while in the catalog to send the item directly to your pockets!");
-        }
-        menu += folder;
+            new MenuEntry("Nookling Upgrades", nullptr, NooklingKeyboard, "Press the keyboard icon to change which upgrade the Nooklings have"),
+            new MenuEntry("Fill out Catalog", FillCatalog, "Fill out the catalog in Nookling's shop"),
+            new MenuEntry("Fill out Main Street", FillMainStreet, "Unlocks all the Main Street buildings except Leif + Nooklings"),
+            new MenuEntry("Catalog to Pockets", CatalogToPockets, "Press " FONT_L " and " FONT_A " while in the catalog to send the item directly to your pockets!")
+        }));
 
         /*
         ** Inventory
         ********************/
 
-        folder = new MenuFolder("Inventory");
+        menu += new MenuFolder("Time Travel Codes", std::vector<MenuEntry *>(
         {
-            *folder += new MenuEntry("Text to Item", Text2Item, "Press " FONT_X " and " FONT_DR " to open the keyboard to enter in the ID you want to recieve.");
-            *folder += new MenuEntry("Duplicate", Duplication, "Press " FONT_R " to duplicate the item that is slot 01 to slot 02.");
-            *folder += new MenuEntry("Show names of buried items", ShowBuriedItems, "This allows you to view what is buried underground in the little thought bubble above your head\nWarning: this is a heavy cheat, so it might cause slowdown.");
-            *folder += new MenuEntry("Pick up buried items", PickBuriedItems, "Press " FONT_Y " to pick up any buried items.\nWarning: this is a heavy cheat, so it might cause slowdown.");
-            *folder += new MenuEntry("Inventory box extender", ExtendedInventoryBox, "This allows you to create 10 additionals boxes to store your items.\nOnce activated, open the quick menu in-game to see the option Inventory Box.");
-            *folder += new MenuEntry("Fossil Inspector", GenerateFossils, "Press " FONT_X " and " FONT_A " to process all fossils\nas if you talked to Blathers");
-            *folder += new MenuEntry("Max Bank", MaxMoneyBank);
-            *folder += new MenuEntry("Infinite Wallet", InfiniteWallet);
-            *folder += new MenuEntry("Infinite/Max Coupons", InfiniteCoupons);
-            *folder += new MenuEntry("Infinite/Max Island Medals", InfiniteMedals);
-            *folder += new MenuEntry("Wallet editor (0)", WalletEditor, WalletEditorSetter, "Touch the keyboard icon on the bottom screen to change the desired value");
-            *folder += new MenuEntry("Bank editor (0)", BankEditor, BankEditorSetter, "Touch the keyboard icon on the bottom screen to enter the desired amount of bells in your bank.");
-        }
-        menu += folder;
+            new MenuEntry("Time Travel", TimeTravel, "Press either " FONT_R " or " FONT_B " and " FONT_DR " to travel forward or " FONT_DL " to retwind time or " FONT_B " and " FONT_DD " to set ingame time back to your 3DS's clock."),
+            new MenuEntry("Time Machine", TimeMachine, "Press " FONT_Y " and " FONT_DR " to start time traveling.")
+        }));
+
+        menu += new MenuFolder("Inventory", std::vector<MenuEntry *>(
+        {
+            new MenuEntry("Text to Item", Text2Item, "Press " FONT_X " and " FONT_DR " to open the keyboard to enter in the ID you want to recieve."),
+            new MenuEntry("Duplicate", Duplication, "Press " FONT_R " to duplicate the item that is slot 01 to slot 02."),
+            new MenuEntry("Show names of buried items", ShowBuriedItems, "This allows you to view what is buried underground in the little thought bubble above your head\nWarning: this is a heavy cheat, so it might cause slowdown."),
+            new MenuEntry("Pick up buried items", PickBuriedItems, "Press " FONT_Y " to pick up any buried items.\nWarning: this is a heavy cheat, so it might cause slowdown."),
+            new MenuEntry("Inventory box extender", ExtendedInventoryBox, "This allows you to create 10 additionals boxes to store your items.\nOnce activated, open the quick menu in-game to see the option Inventory Box."),
+            new MenuEntry("Fossil Inspector", GenerateFossils, "Press " FONT_X " and " FONT_A " to process all fossils\nas if you talked to Blathers"),
+            new MenuEntry("Max Bank", MaxMoneyBank),
+            new MenuEntry("Infinite Wallet", InfiniteWallet),
+            new MenuEntry("Infinite/Max Coupons", InfiniteCoupons),
+            new MenuEntry("Infinite/Max Island Medals", InfiniteMedals),
+            new MenuEntry("Wallet editor (0)", WalletEditor, WalletEditorSetter, "Touch the keyboard icon on the bottom screen to change the desired value"),
+            new MenuEntry("Bank editor (0)", BankEditor, BankEditorSetter, "Touch the keyboard icon on the bottom screen to enter the desired amount of bells in your bank.")
+        }));
 
         /*
         ** Environment
         ********************/
 
-        folder = new MenuFolder("Enviroment");
+        MenuFolder *folder = new MenuFolder("Enviroment");
         {
             /* Subfolder of Enviroment
             ** These codes only execute when R+A is pressed, so I only want 1 enabled.
             ** I use the radio group parameter to prevent multiple from being enabled.
             *****************************************************************************/
-            MenuFolder  *ra = new MenuFolder("R + A Codes");
+            *folder += new MenuFolder("R + A Codes", std::vector<MenuEntry *>(
             {
-                *ra += new MenuEntry(1, "Remove All Items", RemoveAllItems, "Press " FONT_R " and " FONT_A " to execute... Beware as there is no going back if you save.");
-                *ra += new MenuEntry(1, "Remove All Weeds", RemoveAllWeeds, "Press " FONT_R " and " FONT_A " to execute.");
-                *ra += new MenuEntry(1, "Water All Flowers", WaterAllFlowers, "Press " FONT_R " and " FONT_A "to execute.");
-                *ra += new MenuEntry(1, "Fill Grass", FillGrass, "Press " FONT_R " and " FONT_A " to fill your town with grass.\nPlease note that bald spots will respawn on the next day.");
-                *ra += new MenuEntry(1, "Destroy Grass", DestroyGrass, "Press " FONT_R " and " FONT_A " to destroy all the grass in your town to make a desert wasteland.");
-            }
-            *folder += ra;
+                new MenuEntry(1, "Remove All Items", RemoveAllItems, "Press " FONT_R " and " FONT_A " to execute... Beware as there is no going back if you save."),
+                new MenuEntry(1, "Remove All Weeds", RemoveAllWeeds, "Press " FONT_R " and " FONT_A " to execute."),
+                new MenuEntry(1, "Water All Flowers", WaterAllFlowers, "Press " FONT_R " and " FONT_A "to execute."),
+                new MenuEntry(1, "Fill Grass", FillGrass, "Press " FONT_R " and " FONT_A " to fill your town with grass.\nPlease note that bald spots will respawn on the next day."),
+                new MenuEntry(1, "Destroy Grass", DestroyGrass, "Press " FONT_R " and " FONT_A " to destroy all the grass in your town to make a desert wasteland.")
+            }));
             *folder += new MenuEntry("Real Time World Edit", WorldEdit, "Press " FONT_R " and " FONT_DL " to open the keyboard to store the item. " FONT_R " and " FONT_DU " to store the item that you're standing on. And " FONT_R " + " FONT_DD " to write the item to the place that you're standing on.");
             *folder += new MenuEntry("Search and Replace", nullptr, SearchReplace, "Press the keyboard icon to enter in what you want to search and replace");
         }
@@ -212,41 +215,32 @@ namespace CTRPluginFramework
         ** Time Travel
         ********************/
 
-        folder = new MenuFolder("Time Travel Codes");
+       menu += new MenuFolder("Unlock Codes", std::vector<MenuEntry *>(
         {
-            *folder += new MenuEntry("Time Travel", TimeTravel, "Press either " FONT_R " or " FONT_B " and " FONT_DR " to travel forward or " FONT_DL " to retwind time or " FONT_B " and " FONT_DD " to set ingame time back to your 3DS's clock.");
-            *folder += new MenuEntry("Time Machine", TimeMachine, "Press " FONT_Y " and " FONT_DR " to start time traveling.");
-        }
-        menu += folder;
-
-        folder = new MenuFolder("Unlock Codes");
-        {
-            *folder += new MenuEntry("100% Mayor permit", Permit, "Special thanks to Slattz");
-            *folder += new MenuEntry("Unlock all PWPs", PWPUnlock, "Special thanks to Mega Mew and Scotline");
-            *folder += new MenuEntry("Fill out encyclopedia", Encyclopedia, "Special thanks to Mega Mew and Scotline");
-            *folder += new MenuEntry("Fill out emoticons", Emoticons, "Special thanks to Mega Mew and Scotline");
-            *folder += new MenuEntry("Fill out K.K. Songs", Songs, "Special thanks to Mega Mew and Scotline");
-        }
-        menu += folder;
+            new MenuEntry("100% Mayor permit", Permit, "Special thanks to Slattz"),
+            new MenuEntry("Unlock all PWPs", PWPUnlock, "Special thanks to Mega Mew and Scotline"),
+            new MenuEntry("Fill out encyclopedia", Encyclopedia, "Special thanks to Mega Mew and Scotline"),
+            new MenuEntry("Fill out emoticons", Emoticons, "Special thanks to Mega Mew and Scotline"),
+            new MenuEntry("Fill out K.K. Songs", Songs, "Special thanks to Mega Mew and Scotline")
+       }));
 
         /*
         ** Misc.
         ********************/
 
-        folder = new MenuFolder("Misc.");
+        menu += new MenuFolder("Misc.", std::vector<MenuEntry *>(
         {
-            *folder += new MenuEntry("Ghost Mode", GhostMode, "Press Y + D pad up to enable, Y + D pad down to disable");
-            *folder += new MenuEntry("Camera Mod", CameraMod, "R + Circle pad = rotation \n\n R + A stop player from moving or Y lock camera, X unlock camera(needed) \n\n B + D pad move camera, L / R up and down");
-            *folder += new MenuEntry("Custom Symbols Keyboard", CustomKB, "This turns all the symbols in the keyboard into Nintendo symbols.\nExample: \uE00F\uE004\uE000\uE00E\uE00E\uE04B");
-            *folder += new MenuEntry("Keyboard Extender", KeyboardExtender, "This extends the max characters that you can type into chat to 54 characters.\nSpecial thanks to Wii8461 for this cheat");
-            *folder += new MenuEntry("Fast Game Speed", FastGameSpeed, "This makes things in the game speed up. This might make your game crash.\nCredits to Scotline and Mega Mew for this cheat");
-            *folder += new MenuEntry("Item Form Changer", ItemFormChanger, ItemFormEditor, "This changes how your character holds tools");
-            *folder += new MenuEntry("Item Effect Changer", ItemEffectChanger, ItemEffectEditor, "This changes how your character uses items.");
-            *folder += new MenuEntry("Special NPC Changer", AnimalChanger, AnimalChangerKeyboard, "This changes all the special NPC's like K.K. to what you choose.");
-            *folder += new MenuEntry("Access Catalog & Storage Anywhere", StorageEverywhere, "Press L or R to access your storage, and press L+R to access the catalog while switching emoticon tabs\nSpecial thanks Mega Mew and Scotline for this cheat :)");
-            *folder += new MenuEntry("Faint", Faint, "Press R + A to make your character pass out like he got bit by a scorpion!\nCredits to Hikaru");
-        }
-        menu += folder;
+            new MenuEntry("Ghost Mode", GhostMode, "Press Y + D pad up to enable, Y + D pad down to disable"),
+            new MenuEntry("Camera Mod", CameraMod, "R + Circle pad = rotation \n\n R + A stop player from moving or Y lock camera, X unlock camera(needed) \n\n B + D pad move camera, L / R up and down"),
+            new MenuEntry("Custom Symbols Keyboard", CustomKB, "This turns all the symbols in the keyboard into Nintendo symbols.\nExample: \uE00F\uE004\uE000\uE00E\uE00E\uE04B"),
+            new MenuEntry("Keyboard Extender", KeyboardExtender, "This extends the max characters that you can type into chat to 54 characters.\nSpecial thanks to Wii8461 for this cheat"),
+            new MenuEntry("Fast Game Speed", FastGameSpeed, "This makes things in the game speed up. This might make your game crash.\nCredits to Scotline and Mega Mew for this cheat"),
+            new MenuEntry("Item Form Changer", ItemFormChanger, ItemFormEditor, "This changes how your character holds tools"),
+            new MenuEntry("Item Effect Changer", ItemEffectChanger, ItemEffectEditor, "This changes how your character uses items."),
+            new MenuEntry("Special NPC Changer", AnimalChanger, AnimalChangerKeyboard, "This changes all the special NPC's like K.K. to what you choose."),
+            new MenuEntry("Access Catalog & Storage Anywhere", StorageEverywhere, "Press L or R to access your storage, and press L+R to access the catalog while switching emoticon tabs\nSpecial thanks Mega Mew and Scotline for this cheat :)"),
+            new MenuEntry("Faint", Faint, "Press R + A to make your character pass out like he got bit by a scorpion!\nCredits to Hikaru")
+        }));
 
         /*
         ** Callbacks
