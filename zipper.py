327ee9dbea8237d12a5bed43820a13f3a488ad24
Ryan <rplante565@gmail.com>
2020-01-06 19:41:31 -0500

    fix zipper script

--- zipper.py
+++ zipper.py
@@ -4,19 +4,15 @@ import os
 import datetime
 JAP_TID_LOW = 86200
 D_ASCII = 68
-FILE        = "ACNL-NTR-Cheats.plg"
+FILE        = "ACNL-NTR-Cheats.3gx"
 def printf(string):
     print(datetime.datetime.strftime(datetime.datetime.now(), '%Y-%m-%d %H:%M:%S') + " : " + string)
 if not os.path.exists("plugin"):
     for i in range(0, 3):
-        os.makedirs("plugin/plugin/" + "00040000000" + str(JAP_TID_LOW + (i * 100)))
-        os.makedirs("plugin/plugin/" + "0004000000198" + chr(D_ASCII + i) + "00")
         os.makedirs("plugin/luma/plugins/" + "00040000000" + str(JAP_TID_LOW + (i * 100)))
         os.makedirs("plugin/luma/plugins/" + "0004000000198" + chr(D_ASCII + i) + "00")
 printf("Copying files into each folder")
 for i in range(0, 3):
-    shutil.copy2(FILE, "plugin/plugin/" + "00040000000" + str(JAP_TID_LOW + (i * 100)) + "/" + FILE)
-    shutil.copy2(FILE, "plugin/plugin/" + "0004000000198" + chr(D_ASCII + i) + "00" + "/" + FILE)
     shutil.copy2(FILE, "plugin/luma/plugins/" + "00040000000" + str(JAP_TID_LOW + (i * 100)) + "/" + FILE)
     shutil.copy2(FILE, "plugin/luma/plugins/" + "0004000000198" + chr(D_ASCII + i) + "00" + "/" + FILE)
 shutil.make_archive("ACNL-NTR-Cheats", 'zip', r".\\plugin")
