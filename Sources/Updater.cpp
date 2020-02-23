ff732020832b5286da08b6fb77299a42de49eba4
unknown <rplante565@gmail.com>
2020-02-23 00:58:52 -0500

    update updater string

--- Sources/Updater.cpp
+++ Sources/Updater.cpp
@@ -219,7 +219,7 @@ namespace CTRPluginFramework
 			else return false; //if no update
 
 
-			Sstrncpy(urlDownload, "https://raw.githubusercontent.com/rydoginator/ACNL-NTR-Cheats/master/ACNL-NTR-Cheats.plg", 200);
+			Sstrncpy(urlDownload, "https://raw.githubusercontent.com/rydoginator/ACNL-NTR-Cheats/master/ACNL-NTR-Cheats.3gx", 200);
 			Sstrncpy(newChangelog, "The changelog cannot be viewed from the plugin.\nVisit < http://bit.ly/ACPLG-Change >\nto see the changelog on Github.", 200);
 
 			return updateavailable;
